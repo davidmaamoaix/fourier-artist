00f242247bf21f73fdb1eb108636a0409cdef013
BuildTools <davidmaamoaix@gmail.com>
2021-10-24 09:51:30 +0800

    some tweaks

--- assets/js/canvas.js
+++ assets/js/canvas.js
@@ -4,34 +4,91 @@ const canvasInit = function() {
     var series = [];
     var record = [];
 
+    var mouseDown = false;
+    var mousePos = [0, 0];
+    var drawPath = [];
+    var showLine = true;
+
     function start() {
         const canvas = document.getElementById("canvas");
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
 
+        canvas.addEventListener("mousedown", e => {
+            drawPath = [];
+            mouseDown = true;
+        });
+        canvas.addEventListener("mouseup", e => {
+            mouseDown = false;
+            series = fourier.distTransform(drawPath);
+            series.sort((a, b) => b[1] - a[1]);
+
+            record = [];
+            time = 0;
+        });
+        canvas.addEventListener("mousemove", e => {
+            const rect = canvas.getBoundingClientRect();
+            mousePos = [
+                e.clientX - rect.left - canvas.width / 2,
+                e.clientY - rect.top - canvas.height / 2
+            ];
+        });
+        window.addEventListener("keydown", e => {
+            if (event.keyCode === 32) {
+                showLine = !showLine;
+            } else if (event.keyCode === 82) {
+                drawPath = [];
+                series = [];
+                record = [];
+                time = 0;
+            }
+        });
+
         ctx = canvas.getContext("2d");
         setInterval(update, 25);
+        setInterval(input, 10);
+    }
 
-        const temp = [];
-        for (var i = -225; i < 225; i += 2) {
-            temp.push([i, -225]);
+    function input() {
+        if (mouseDown) {
+            drawPath.push(mousePos);
         }
-        for (var i = -225; i < 225; i += 2) {
-            temp.push([i, 225]);
-        }
+    }
 
-        series = fourier.distTransform(temp);
-        series.sort((a, b) => b[1] - a[1]);
+    function drawInput(midX, midY) {
+        if (drawPath.length > 0) {
+            if (mouseDown) {
+                ctx.strokeStyle = "#FFF"
+                ctx.lineWidth = 5;
+            } else {
+                if (!showLine) return;
+
+                ctx.strokeStyle = "#AAA"
+                ctx.lineWidth = 2;
+            }
+
+            ctx.beginPath();
+            ctx.moveTo(drawPath[0][0] + midX, drawPath[0][1] + midY);
+            for (var i = 1; i < drawPath.length; i++) {
+                ctx.lineTo(drawPath[i][0] + midX, drawPath[i][1] + midY);
+            }
+
+            ctx.stroke();
+        }
     }
 
     function update() {
-        if (series.length === 0) return;
-
-        const [midX, midY] = [canvas.width / 2, canvas.height / 2];
-        const circles = [];
-        let [sX, sY] = [canvas.width / 2, canvas.height / 2];
 
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+        const [midX, midY] = [canvas.width / 2, canvas.height / 2];
+
+        drawInput(midX, midY);
+
+        // skip fourier
+        if (series.length === 0) return;
+
+        const circles = [];
+        let [sX, sY] = [canvas.width / 2, canvas.height / 2];
 
         ctx.strokeStyle = "#FFF";
         ctx.lineWidth = 1;
@@ -70,7 +127,8 @@ const canvasInit = function() {
         if (record.length > Math.max(40, series.length - 20)) record.pop();
 
         // offset by period
-        time += Math.PI * 2 / series.length * 1;
+        // negate to match the draw order
+        time -= Math.PI * 2 / series.length * 1;
     }
 
     window.onload = start;
